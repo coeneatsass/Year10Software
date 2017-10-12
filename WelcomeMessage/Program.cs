04dee5318d4907417ce4d2536a508470c02cd1b3
Coen Sentance <Coen.Sentance@education.nsw.gov.au>
2017-10-12 12:39:37 +1100

    made hello world and welcome message

---
+++ WelcomeMessage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.Write("enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Welcome To My Porn Website " + name);
+
+
+            //wait at the end
+            Console.ReadKey();
+        }
+    }
+}
