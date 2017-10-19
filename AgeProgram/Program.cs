a347d44678803b714c83e6f35b78027f8f470307
Coen Sentance <Coen.Sentance@education.nsw.gov.au>
2017-10-20 09:47:27 +1100

    finished age preogram

---
+++ AgeProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 13)
+            {
+
+                Console.WriteLine("You are a crossant");
+            }
+            else if (age < 20)
+            {
+
+                Console.WriteLine("You are a fgt");
+            }
+            else
+            {
+                Console.Write("yeah boiii");
+
+
+
+            }
+            Console.ReadKey();
+        }   
+            
+            
+            
+             
+    }
+}
